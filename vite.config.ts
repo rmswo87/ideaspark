7081e81da42e92f7e9bee1275b0d23209d0565f8
rmswo87 <56644355+rmswo87@users.noreply.github.com>
2025-11-26 23:08:53 +0900

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // Vercel 배포 후 vercel dev 사용 시 프록시 불필요
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
+})
